dbb6dd7f98fe5171ba903aba2cd2d8e4ce4ba046
David Oliveros <dato.oliveros@gmail.com>
2014-02-09 02:04:43 -0800

    Wrapped in  to avoid element's position not being defined on link

--- sticky.js
+++ sticky.js
@@ -1,51 +1,54 @@
 angular.module('sticky', [])
 
-.directive('sticky', function(){
+.directive('sticky', function($timeout){
 	return {
 		restrict: 'A',
 		scope: {
 			offset: '@',
 		},
 		link: function($scope, $elem, $attrs){
-			var offsetTop = $scope.offset || 0,
-				$window = angular.element(window),
-				doc = document.documentElement,
-				initialPositionStyle = $elem.css('position'),
-				stickyLine,
-				scrollTop;
-
-			// Set the top offset
-			//
-			$elem.css('top', offsetTop+'px');
+			$timeout(function(){
+				var offsetTop = $scope.offset || 0,
+					$window = angular.element(window),
+					doc = document.documentElement,
+					initialPositionStyle = $elem.css('position'),
+					stickyLine,
+					scrollTop;
 
 
-			// Get the sticky line
-			//
-			function setInitial(){
-				$elem.css('position', initialPositionStyle);
-				stickyLine = $elem[0].offsetTop - offsetTop;
-				checkSticky();
-			}
+				// Set the top offset
+				//
+				$elem.css('top', offsetTop+'px');
 
-			// Check if the window has passed the sticky line
-			//
-			function checkSticky(){
-				scrollTop = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 
-				if ( scrollTop >= stickyLine ){
-					$elem.css('position', 'fixed');
-				} else {
-					$elem.css('position', initialPositionStyle);
+				console.log(initialPositionStyle);
+				// Get the sticky line
+				//
+				function setInitial(){
+					console.log($elem[0].offsetTop);
+					stickyLine = $elem[0].offsetTop - offsetTop;
+					checkSticky();
 				}
-			}
 
-			// Attach our listeners
-			//
-			$window.on('scroll', checkSticky);
-			$window.on('resize', setInitial);
-			
+				// Check if the window has passed the sticky line
+				//
+				function checkSticky(){
+					scrollTop = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 
-			setInitial();
+					if ( scrollTop >= stickyLine ){
+						$elem.css('position', 'fixed');
+					} else {
+						$elem.css('position', initialPositionStyle);
+					}
+				}
+
+				// Attach our listeners
+				//
+				$window.on('scroll', checkSticky);
+				$window.on('resize', setInitial);
+				
+				setInitial();
+			});
 		},
 	};
 })

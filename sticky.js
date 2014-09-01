a247fd7a47670f17f2ae8c5dc37b868a8c9dc7bf
Anatoliy Stegniy <tsdaemon@gmail.com>
2014-09-01 17:08:19 +0300

    Identation fixed

--- sticky.js
+++ sticky.js
@@ -1,15 +1,15 @@
 angular.module('sticky', [])
 
 .directive('sticky', ['$timeout',
-	function ($timeout) {
-	    return {
-	        restrict: 'A',
-	        scope: {
-	            offset: '@',
-	        },
-	        link: function ($scope, $elem, $attrs) {
-	            $timeout(function () {
-	                var offsetTop = $scope.offset || 0,
+	function($timeout) {
+		return {
+			restrict: 'A',
+			scope: {
+				offset: '@',
+			},
+			link: function($scope, $elem, $attrs) {
+				$timeout(function() {
+					var offsetTop = $scope.offset || 0,
 						$window = angular.element(window),
 						doc = document.documentElement,
 						initialPositionStyle = $elem.css('position'),
@@ -19,51 +19,51 @@ angular.module('sticky', [])
 						left;
 
 
-	                // Set the top offset
-	                //
-	                $elem.css('top', offsetTop + 'px');
+					// Set the top offset
+					//
+					$elem.css('top', offsetTop + 'px');
 
 
-	                // Get the sticky line
-	                //
-	                function setInitial() {
-	                    console.log($elem[0]);
-	                    stickyLine = $elem[0].offsetTop - offsetTop;
-	                    checkSticky();
-	                    left = $elem[0].offsetLeft + $elem[0].clientWidth;
-	                }
+					// Get the sticky line
+					//
+					function setInitial() {
+						console.log($elem[0]);
+						stickyLine = $elem[0].offsetTop - offsetTop;
+						checkSticky();
+						left = $elem[0].offsetLeft + $elem[0].clientWidth;
+					}
 
-	                // Check if the window has passed the sticky line
-	                //
-	                function checkSticky() {
-	                    scrollTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+					// Check if the window has passed the sticky line
+					//
+					function checkSticky() {
+						scrollTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
 
-	                    if (scrollTop >= stickyLine) {
-	                        $elem.css('position', 'fixed');
-	                        $elem.css('left', left + 'px');
-	                    } else {
-	                        $elem.css('position', initialPositionStyle);
-	                        $elem.css('left', initialLeft);
-	                    }
-	                }
-
-
-	                // Handle the resize event
-	                //
-	                function resize() {
-	                    $elem.css('position', initialPositionStyle);
-	                    $timeout(setInitial);
-	                }
+						if (scrollTop >= stickyLine) {
+							$elem.css('position', 'fixed');
+							$elem.css('left', left + 'px');
+						} else {
+							$elem.css('position', initialPositionStyle);
+							$elem.css('left', initialLeft);
+						}
+					}
 
 
-	                // Attach our listeners
-	                //
-	                $window.on('scroll', checkSticky);
-	                $window.on('resize', resize);
+					// Handle the resize event
+					//
+					function resize() {
+						$elem.css('position', initialPositionStyle);
+						$timeout(setInitial);
+					}
 
-	                setInitial();
-	            });
-	        },
-	    };
+
+					// Attach our listeners
+					//
+					$window.on('scroll', checkSticky);
+					$window.on('resize', resize);
+
+					setInitial();
+				});
+			},
+		};
 	}
 ]);
